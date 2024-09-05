aded86ce9a2edd67a96155ea387df48379d28a4c
sakura11107 <ltf1409417317@Gmail.com>
2024-09-05 17:39:59 +0800

    处理/

--- server/app.py
+++ server/app.py
@@ -5,7 +5,11 @@ from auth import auth , friends_bp
 from openaigpt import chatgpt_bp
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost/chatonline'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
